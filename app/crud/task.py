79a9e064738215a86a1ebd9f041b3a6ca5b35376
Hamed <hamedasgari20@yahoo.com>
2023-11-03 19:07:25 +0330

    Refactoring

--- app/crud/task.py
+++ app/crud/task.py
@@ -1,58 +1,25 @@
-from app.db.database import SessionLocal
+from sqlalchemy.orm import Session
+
 from app.models.task import Task
 from app.schemas.task import TaskResponse
 
 
-def read_task_crud(task_id: int):
-    """
-    Retrieve a task from the database by ID.
-
-    Args:
-        task_id (int): The ID of the task to retrieve.
-
-    Returns:
-        Task: The task with the specified ID, or None if no such task exists.
-    """
-    db = SessionLocal()
+def read_task_crud(db: Session, task_id: int):
     task = db.query(Task).filter(Task.id == task_id).first()
-    db.close()
     return task
 
 
-def create_task_crud(task: TaskResponse):
-    """
-    Create a new task in the database.
-
-    Args:
-        task (TaskResponse): The details of the task to create.
-
-    Returns:
-        Task: The newly created task.
-    """
-    db = SessionLocal()
+def create_task_crud(db: Session, task: TaskResponse):
     db_task = Task(**task.dict())
     db.add(db_task)
     db.commit()
     db.refresh(db_task)
-    db.close()
     return db_task
 
 
-def update_task_crud(task_id: int, updated_task: TaskResponse):
-    """
-    Update an existing task in the database.
-
-    Args:
-        task_id (int): The ID of the task to update.
-        updated_task (TaskResponse): The updated details of the task.
-
-    Returns:
-        Task: The updated task, or None if no such task exists.
-    """
-    db = SessionLocal()
-    task = read_task_crud(task_id)
+def update_task_crud(db: Session, task_id: int, updated_task: TaskResponse):
+    task = read_task_crud(db, task_id)
     if task is None:
-        db.close()
         return None
     for key, value in updated_task.dict(exclude_unset=True).items():
         setattr(task, key, value)
@@ -63,18 +30,8 @@ def update_task_crud(task_id: int, updated_task: TaskResponse):
 
 
 #
-def delete_task_crud(task_id: int):
-    """
-    Delete a task from the database.
-
-    Args:
-        task_id (int): The ID of the task to delete.
-
-    Returns:
-        Task: The deleted task, or None if no such task exists.
-    """
-    db = SessionLocal()
-    task = read_task_crud(task_id)
+def delete_task_crud(db: Session, task_id: int):
+    task = read_task_crud(db, task_id)
     if task is None:
         db.close()
         return None
--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi import HTTPException
+from sqlalchemy.orm import Session
+
 from app.crud.task import create_task_crud, read_task_crud, update_task_crud, delete_task_crud
 from app.db.database import SessionLocal
 from app.schemas.task import TaskResponse
@@ -16,29 +18,29 @@ def get_db():
 
 
 @app.post("/tasks/", response_model=TaskResponse)
-def create_task(task: TaskResponse):
-    return create_task_crud(task)
+def create_task(task: TaskResponse, db: Session = Depends(get_db)):
+    return create_task_crud(db, task)
 
 
 @app.get("/tasks/{task_id}", response_model=TaskResponse)
-def read_task(task_id: int):
-    task = read_task_crud(task_id)
+def read_task(task_id: int, db: Session = Depends(get_db)):
+    task = read_task_crud(db, task_id)
     if task is None:
         raise HTTPException(status_code=404, detail="Task not found")
     return task
 
 
 @app.put("/tasks/{task_id}", response_model=TaskResponse)
-def update_task(task_id: int, updated_task: TaskResponse):
-    task = update_task_crud(task_id, updated_task)
+def update_task(task_id: int, updated_task: TaskResponse, db: Session = Depends(get_db)):
+    task = update_task_crud(db, task_id, updated_task)
     if task is None:
         raise HTTPException(status_code=404, detail="Task not found")
     return task
 
 
 @app.delete("/tasks/{task_id}", response_model=TaskResponse)
-def delete_task(task_id: int):
-    task = delete_task_crud(task_id)
+def delete_task(task_id: int, db: Session = Depends(get_db)):
+    task = delete_task_crud(db, task_id)
     if task is None:
         raise HTTPException(status_code=404, detail="Task not found")
     return task
