3c51a2397e327de2c5b412eaaf9c6eeabede0265
Hamed <hamedasgari20@yahoo.com>
2023-11-16 15:34:44 +0330

    Adding authentication

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 
+from app.db.auth import create_db_and_tables
 from app.routers.tasks import router as tasks_router
+from app.routers.auth import router as auth_router
 
 app = FastAPI()
 
-app.include_router(tasks_router, prefix="/task", tags=["task"],)
+app.include_router(tasks_router, prefix="/task", tags=["task"])
+app.include_router(auth_router, prefix="/auth/jwt", tags=["auth"])
+
+
+@app.on_event("startup")
+async def on_startup():
+    # Not needed if you setup a migration system like Alembic
+    await create_db_and_tables()
